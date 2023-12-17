bae16a5fadd48de803b8efa3064f0bc6d0246118
kalmenn <kale@kalmenn.fr>
2023-12-17 19:01:54 +0100

    nvim: neotree: added keymaps

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,7 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    lazy = false,
     config = function()
         require('neo-tree').setup({
             filesystem = {
@@ -18,4 +19,10 @@ return {
             },
         })
     end,
+    keys = {
+        { "<C-N>t", "<cmd>Neotree toggle<cr>", "Toggle Neotree" },
+        { "<C-N>f", "<cmd>Neotree filesystem focus<cr>", "Focus filesystem in Neotree" },
+        { "<C-N>b", "<cmd>Neotree buffers focus<cr>", "Focus buffers in Neotree" },
+        { "<C-N>g", "<cmd>Neotree git_status focus<cr>", "Focus git status in Neotree" },
+    },
 }
