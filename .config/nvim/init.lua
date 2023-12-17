9d4cbb845c482e00277a863a5f2fa2891e426d20
kalmenn <kale@kalmenn.fr>
2023-12-17 02:18:19 +0100

    nvim: init.lua: switched from nvim-tree to neo-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,25 @@ require('lazy').setup({
     "catppuccin/nvim",
     "nvim-treesitter/nvim-treesitter",
     "neovim/nvim-lspconfig",
-    "nvim-tree/nvim-tree", -- TODO: replace with neo-tree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        config = function ()
+            require('neo-tree').setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = true,
+                    },
+                },
+            })
+        end,
+    },
 })
 
 --[[
@@ -47,25 +65,6 @@ require('nvim-treesitter.configs').setup {
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
 lspconfig.rust_analyzer.setup {}
-
--- enable highlight groups
-vim.opt.termguicolors = true
-
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 30,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = false,
-    },
-    sync_root_with_cwd = true,
-})
 
 vim.keymap.set('n', '<A-k>', ':move -2<CR>' )
 vim.keymap.set('n', '<A-up>', ':move -2<CR>' )
