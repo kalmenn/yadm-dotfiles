048c90fde41ad1dd596fe21d975de7e211dfa4b5
kalmenn <kale@kalmenn.fr>
2023-12-18 00:30:23 +0100

    nvim: feline: enabled the status line to integrate with gitsigns

--- .config/nvim/lua/plugins/feline.lua
+++ .config/nvim/lua/plugins/feline.lua
@@ -4,7 +4,7 @@ return {
     config = function(_, opts)
         require('feline').setup()
         -- require('feline').winbar.setup()       -- to use winbar
-        -- require('feline').statuscolumn.setup() -- to use statuscolumn
+        require('feline').statuscolumn.setup() -- to use statuscolumn
 
         -- require('feline').use_theme()          -- to use a custom theme
     end
