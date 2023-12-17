1a9ae8abeffa7ae0929497e0980d187f72569d74
kalmenn <kale@kalmenn.fr>
2023-12-18 00:26:46 +0100

    nvim: sets: auto close unused buffers

--- .config/nvim/lua/sets.lua
+++ .config/nvim/lua/sets.lua
@@ -4,36 +4,32 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 -- show hidden characters
-
 vim.opt.list = true
 vim.opt.listchars = { tab = '→ ', trail = '·' }
 
 -- indentation
-
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = vim.opt.tabstop:get()
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-
 vim.opt.smartindent = true
 
 -- line wrapping
-
 vim.opt.wrap = false
 
 -- undo history
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
 vim.opt.undofile = true
-
 vim.opt.updatetime = 50
 
 -- end search when exiting
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+-- auto-close unused buffers if all changes have been saved
+vim.opt.hidden = false
 
 -- stuff
 
